07f65b6dfaf98ed495b013bd4bb0f186e6c0f769
AZagatti <andre.zagatti1@gmail.com>
2019-08-27 01:04:45 -0300

    Includes ESlint, Prettier and EditorConfig

--- src/services/api.js
+++ src/services/api.js
@@ -12,8 +12,7 @@ export function loadLists() {
         },
         {
           id: 2,
-          content:
-            'Criar vídeo para o Youtube ensinando a recriar a interface do Pipefy',
+          content: 'Recriar a interface do Pipefy',
           labels: ['#7159c1'],
           user: 'https://avatars0.githubusercontent.com/u/40201172?v=4',
         },
@@ -25,14 +24,13 @@ export function loadLists() {
         },
         {
           id: 4,
-          content:
-            'Gravar Aula "NextJS: Utilizando server-side rendering com ReactJS"',
+          content: 'NextJS: Utilizando server-side rendering com ReactJS',
           labels: ['#54e1f7'],
           user: 'https://avatars0.githubusercontent.com/u/40201172?v=4',
         },
         {
           id: 5,
-          content: 'Gravar testes e deploy ReactJS',
+          content: 'Testes e deploy ReactJS',
           labels: ['#54e1f7'],
           user: 'https://avatars0.githubusercontent.com/u/40201172?v=4',
         },
@@ -56,19 +54,19 @@ export function loadLists() {
       cards: [
         {
           id: 7,
-          content: 'Gravar sobre Geolocalização e mapas com React Native',
+          content: 'Geolocalização e mapas com React Native',
           labels: ['#7159c1'],
           user: 'https://avatars0.githubusercontent.com/u/40201172?v=4',
         },
         {
           id: 8,
-          content: 'Gravar testes e deploy ReactJS',
+          content: 'Testes e deploy ReactJS',
           labels: ['#54e1f7'],
           user: 'https://avatars0.githubusercontent.com/u/40201172?v=4',
         },
         {
           id: 9,
-          content: 'Ajustes na biblioteca unform',
+          content: 'Ajustes nos códigos do github',
           labels: [],
         },
       ],
@@ -80,18 +78,18 @@ export function loadLists() {
       cards: [
         {
           id: 10,
-          content: 'Gravar aula sobre deploy e CI com React Native',
+          content: 'Deploy e CI com React Native',
           labels: [],
         },
         {
           id: 12,
-          content: 'Gravar testes e deploy ReactJS',
+          content: 'Testes e deploy ReactJS',
           labels: ['#54e1f7'],
         },
         {
           id: 13,
           content:
-            'Gravar Aula "Internacionalização de aplicações Node.js, ReactJS e React Native"',
+            'Internacionalização de aplicações Node.js, ReactJS e React Native',
           labels: ['#7159c1'],
         },
       ],
